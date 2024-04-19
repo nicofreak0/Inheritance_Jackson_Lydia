01fccf69427315da3e60089165d29086fa268bea
Lydia <146027704+nicofreak0@users.noreply.github.com>
2024-04-19 13:05:54 -0500

    fr

---
+++ Game.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Jackson_Lydia
+{
+    abstract class Game
+    {
+
+        public string Esrb { get; set; }
+
+        public string Title { get; set; }
+
+        public Game(string esrbParam, string titleParam)
+        {
+            Esrb = esrbParam;
+            Title = titleParam;
+        }
+
+        public void PlayGame()
+        {
+            Console.WriteLine($"{Title} is Starting.");
+        }
+
+        public abstract string Describe();
+
+    }
+}
