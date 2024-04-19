027fbfab17593be201666f135dd9184c409d7394
Lydia <146027704+nicofreak0@users.noreply.github.com>
2024-04-19 12:34:17 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Inheritance_Jackson_Lydia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Inheritance_Jackson_Lydia
+ 
